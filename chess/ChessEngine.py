03db93333c099e37d51b7acee7960649cb915b4b
Pranav Kumar <pranav.pk.kumar2003@gmail.com>
2023-11-03 14:05:09 -0600

    board setup (part2)

---
+++ chess/ChessEngine.py
@@ -0,0 +1,26 @@
+"""
+
+This is a basic chess engine (wanna be Stockfish)
+functionality and validity of moves are determined by this engine
+
+"""
+
+class gameState():
+
+    def __init__(self) -> None:
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["..", "..", "..", "..", "..", "..", "..", ".."],
+            ["..", "..", "..", "..", "..", "..", "..", ".."],
+            ["..", "..", "..", "..", "..", "..", "..", ".."],
+            ["..", "..", "..", "..", "..", "..", "..", ".."],
+            ["..", "..", "..", "..", "..", "..", "..", ".."],
+            ["..", "..", "..", "..", "..", "..", "..", ".."],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ]
+
+        self.whiteToMove = True
+        self.moveLog = []
+
